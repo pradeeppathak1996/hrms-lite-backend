f8b6856b403c19dc48060ac71dd40201a5ce5a2c
Pradeep Pathak <pradeeppathak@Pradeeps-MacBook-Air.local>
2026-02-24 14:14:10 +0530

    add root health check endpoint

--- hrms/urls.py
+++ hrms/urls.py
@@ -23,15 +23,17 @@
 #     path("api/", include("attendance.urls")),
 # ]
 
-
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+
+def home(request):
+    return JsonResponse({"status": "HRMS Lite Backend is running"})
+
 
 urlpatterns = [
-    # path("admin/", admin.site.urls),
-    # path("api/employees/", include("employees.urls")),
-    # path("api/attendance/", include("attendance.urls")),
-
+    path("", home),  
     path("admin/", admin.site.urls),
     path("api/employees/", include("employees.urls")),
     path("api/attendance/", include("attendance.urls")),
