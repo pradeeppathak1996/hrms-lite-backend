b4cac39d99bd4f39b377aa83aa99c36704d2e257
Pradeep Pathak <pradeeppathak@Pradeeps-MacBook-Air.local>
2026-02-24 14:43:40 +0530

    Fix employee delete 500 error

--- employees/views.py
+++ employees/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Employee
 from .serializers import EmployeeSerializer
 
@@ -6,7 +7,17 @@ class EmployeeListCreateView(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
+
 class EmployeeDeleteView(generics.DestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
-    lookup_field = 'id'
+    lookup_field = 'pk'
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            return super().delete(request, *args, **kwargs)
+        except Employee.DoesNotExist:
+            return Response(
+                {"error": "Employee not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
