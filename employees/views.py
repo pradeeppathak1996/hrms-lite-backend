63713d407aaf1af9760a167c0743118eab1aafce
Pradeep Pathak <pradeeppathak@Pradeeps-MacBook-Air.local>
2026-02-24 15:01:14 +0530

    commited

--- employees/views.py
+++ employees/views.py
@@ -1,4 +1,19 @@
-from rest_framework import generics
+# from rest_framework import generics
+# from .models import Employee
+# from .serializers import EmployeeSerializer
+
+# class EmployeeListCreateView(generics.ListCreateAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
+
+# class EmployeeDeleteView(generics.DestroyAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
+#     lookup_field = 'id'
+
+
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Employee
 from .serializers import EmployeeSerializer
 
@@ -6,7 +21,18 @@ class EmployeeListCreateView(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
-class EmployeeDeleteView(generics.DestroyAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-    lookup_field = 'id'
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "Employee added successfully"},
+                status=status.HTTP_201_CREATED
+            )
+
+        # 👇 THIS IS IMPORTANT
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
